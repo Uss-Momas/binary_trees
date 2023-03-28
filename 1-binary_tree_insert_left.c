fd5fc6f33fefa881035c910a5e430e1d16373c12
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-28 20:12:21 +0200

    insert left child file

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_insert_left - inserts node in the left of the parent
+ * @parent: pointer to the node to insert the left child
+ * @value: the data to insert to the node
+ * Return: the pointer to the new left child node
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+}

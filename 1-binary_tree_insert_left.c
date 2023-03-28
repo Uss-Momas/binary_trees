3f74fda456a08ae23247c9689b2efb0d8643f714
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-28 20:48:17 +0200

    Fixing parent value

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,12 +17,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL || parent == NULL)
 		return (NULL);
 	new_node->n = value;
-	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
 	if (parent->left == NULL)
+	{
 		parent->left = new_node;
+		new_node->parent = parent;
+	}
 	else
 	{
 		left_node = parent->left;

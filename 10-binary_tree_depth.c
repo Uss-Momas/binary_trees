8b078e68cbc056476fa4de01bb1dff16d1fbe21d
Uss-Momas <ussumane.momade@uem.ac.mz>
2023-03-30 20:58:47 +0200

    Implementing the calculation of the depth of a node

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - find the depth of a node
+ * @tree: pointer to the root node
+ * Return: the depth of the node
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+	const binary_tree_t *root_ref;
+
+	if (tree == NULL)
+		return (0);
+	root_ref = tree;
+	while(root_ref->parent != NULL)
+	{
+		root_ref = root_ref->parent;
+		depth++;
+	}
+
+	return (depth);
+}
